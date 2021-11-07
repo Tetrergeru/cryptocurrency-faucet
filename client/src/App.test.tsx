f72a47b8af2b15337d47ad94434cf1d0d967b8dd
Tetrergeru <41305740+Tetrergeru@users.noreply.github.com>
2021-11-07 20:01:16 +0300

    [client] Remove failing test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -7,7 +7,3 @@ test('renders learn react link', () => {
 	const linkElement = screen.getByText(/learn react/i);
 	expect(linkElement).toBeInTheDocument();
 });
-
-test('test designed to fail', () => {
-    fail('Task failed successfully');
-});
