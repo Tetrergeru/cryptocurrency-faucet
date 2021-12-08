345b31dcd39970830f39ed4cffd3727a8b2c2317
Arseniy <38361239+ArseniyAvilov@users.noreply.github.com>
2021-12-08 20:09:52 +0300

    Add files via upload

--- terra/src/terra-faucet.ts
+++ terra/src/terra-faucet.ts
@@ -10,12 +10,12 @@ function createWallet(mnemonic: string, gasType: string, gas: number) {
         gasPrices: gasCoins,
       });
 
-    const wallet = terra.wallet(new MnemonicKey({mnemonic:mnemonic}));
+    const wallet = terra.wallet(new MnemonicKey({mnemonic}));
     return {terra, wallet};
 
 }
 
-async function swapTokens(terraClient: LCDClient, wallet: Wallet, fromTokens: string, toTokens: string, amount: number) {
+async function swapTokens(terraClient: LCDClient, chainId: string, wallet: Wallet, fromTokens: string, toTokens: string, amount: number) {
 
     const swapCoin = new Coin('u'+fromTokens, (1000000*amount).toString());
 
@@ -25,37 +25,40 @@ async function swapTokens(terraClient: LCDClient, wallet: Wallet, fromTokens: st
         'u' + toTokens
       );
       
-      await wallet.createAndSignTx({ msgs: [swap] }).then(tx => terraClient.tx.broadcast(tx))
-      .then(console.info)
-      .catch((err) => {
-        if (err.response) {
-          console.error(err.response.data);
-        } else {
-          console.error(err.message);
-        }
-      });
-    
+    const transaction =  await wallet.createAndSignTx({ msgs: [swap] }).then(tx => terraClient.tx.broadcast(tx));
+      // .then(console.info)
+      // .catch((err) => {
+      //   if (err.response) {
+      //     console.error(err.response.data);
+      //   } else {
+      //     console.error(err.message);
+      //   }
+      // });
+    return `https://finder.terra.money/${chainId}/tx/${transaction.txhash}`;
 }
 
-async function terraSender(terraClient: LCDClient, wallet: Wallet, toWallet: string, tokenType: string, amount: number) {
+async function terraSender(terraClient: LCDClient, chainId: string, wallet: Wallet, toWallet: string, tokenType: string, amount: number) {
 
   const sendingCoins = new Coins([new Coin('u'+tokenType, (1000000*amount).toString())]);
   
   const my_tx = new MsgSend(wallet.key.accAddress, toWallet, sendingCoins);
 
-  await wallet.createAndSignTx({msgs: [my_tx]}).then(tx => terraClient.tx.broadcast(tx))
-  .then(console.info)
-  .catch((err) => {
-    if (err.response) {
-      console.error(err.response.data);
-    } else {
-      console.error(err.message);
-    }
-  });
+  const transaction = await wallet.createAndSignTx({msgs: [my_tx]}).then(tx => terraClient.tx.broadcast(tx));
+  // .then(console.info)
+  // .catch((err) => {
+  //   if (err.response) {
+  //     console.error(err.response.data);
+  //   } else {
+  //     console.error(err.message);
+  //   }
+  // });
+  return `https://finder.terra.money/${chainId}/tx/${transaction.txhash}`;
 }
 
 const mnemonic = "there oil victory surface globe cross column barely food world vendor lizard wish transfer assault strong clerk visit fluid shrimp tobacco renew false defy";
-const {terra, wallet} = createWallet(mnemonic, 'luna', 0.0001);
+const {terra, wallet} = createWallet(mnemonic, 'luna', 0.000001);
+const chain = "bombay-12";
 
 //swapTokens(terra, wallet, 'luna', 'usd', 250);
-terraSender(terra, wallet, "terra1axmdadm204mcyh4vqstgjl59us9tc3cah78s3j", 'luna', 100);
+
+terraSender(terra, chain, wallet, "terra1axmdadm204mcyh4vqstgjl59us9tc3cah78s3j", 'luna', 100).then(t => console.log(t));
